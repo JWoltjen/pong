40f2eeab91afd41bbbc4af0fa389b0396d2a5a0a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-28 10:21:00 -0600

    add handle lose logic

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ import Paddle from './Paddle.js'
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
+
 
 let lastTime 
 function update(time) {
@@ -11,9 +14,30 @@ function update(time) {
     const delta = time - lastTime
     ball.update(delta)
     computerPaddle.update(delta, ball.y)
+
+        if(isLose()){
+           handleLose()
+        }
     }
     lastTime = time
     window.requestAnimationFrame(update)
+}
+
+function isLose(){
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose(){
+    const rect = ball.rect()
+    if(rect.right >= window.innerWidth){
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+
+    }
+    ball.reset()
+    computerPaddle.reset()
 }
 
 
